649e3c6cb6a5902159e2939e4850a5053f033b17
Bang Younghyuk / Hyun SooJi <uniyhbang@gmail.com>
2026-01-12 10:00:20 +0900

    init: jeju gyul site

---
+++ jeju-gyul-site/src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>JEJU GYUL</h1>
+
+      <nav>
+        <ul>
+          <li>
+            소개
+            <ul>
+              <li>브랜드 소개</li>
+              <li>제주 이야기</li>
+            </ul>
+          </li>
+
+         <li>
+          <a href="#products">귤 소개</a>
+          <ul>
+            <li>한라봉</li>
+            <li>레드향</li>
+            <li>천혜향</li>
+          </ul>
+         
+         </li>
+          <li>주문</li>
+          <li>AS</li>
+          <li>추후추가</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
